145aaa09e07320de70a71ca74433b9144275bd3b
handreazza <handreazza@squaretrade.com>
2018-10-09 01:15:59 -0300

    clean up code

--- src/app/modules/dashboard/dashboard.controller.js
+++ src/app/modules/dashboard/dashboard.controller.js
@@ -4,16 +4,8 @@ class DashboardCtrl {
     //initialize dependency injections
     this.dashboardService = dashboardService;
     //initialize controller variables
-    this.input = {};
-    this.columns = ['column1', 'column2', 'column3', 'column4',]
-    this.items = [{
-      column1: 1,
-      column2: 2,
-    },
-    {
-      column1: 2,
-      column2: 3,
-    }];
+    this.columns = ['column1', 'column2', 'column3', 'column4']
+    this.items = [{}];
 
     this.activate();
   }
